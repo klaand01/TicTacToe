3468ef9602adfbccdf45f56892d88921d9ceb564
Klara Andersson <klara.andersson@se.experis.com>
2024-01-23 08:32:36 +0100

    Initial files

---
+++ Boolean.CSharp.Main/Marker.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+namespace Boolean.CSharp.Main
+{
+    public class Marker
+    {
+        
+    }
+}
---
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -0,0 +1,22 @@
+﻿using Boolean.CSharp.Main;
+using NUnit.Framework;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Boolean.CSharp.Test
+{
+    [TestFixture]
+    public class CoreTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
